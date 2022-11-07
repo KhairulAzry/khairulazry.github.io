a522a4080a6d4d3e5b64d5e1add6f0fed6eeba0f
KhairulAzry <mkhairulazry@gmail.com>
2022-11-07 15:36:34 +0800

    Cleaning up code

--- src/pages/ComingSoon.vue
+++ src/pages/ComingSoon.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex w-full h-screen bg-dark">
+  <div class="flex w-full h-screen bg-dark">
     <div class="flex-1 flex flex-wrap items-center justify-center">
       <div class="space-y-2 text-white text-center">
-        <div class="text-h1 font-black uppercase">Coming Soon</div>
+        <div class="text-h3 md:text-h1 font-black uppercase">Coming Soon</div>
         <div>This site is under construction. <br/> Thank you for visiting.</div>
       </div>
     </div>
