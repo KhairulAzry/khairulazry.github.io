f037ab7f4fa435881010e48d1d92b54a8f0c414a
KhairulAzry <mkhairulazry@gmail.com>
2022-11-07 15:36:24 +0800

    Add Homepage

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex w-full h-screen bg-dark">
+      <div class="flex-1 flex flex-wrap items-center justify-center">
+        <div class="space-y-2 text-white text-center">
+          <div class="text-h3 md:text-h1 font-black uppercase">Website Coming Soon</div>
+          <div>This site is under construction. <br/> Thank you for visiting.</div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: 'Home'
+  }
+  </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createWebHistory, createRouter, routerKey } from 'vue-router';
 
 import ComingSoon from '../pages/ComingSoon.vue';
 
+import Home from '../pages/Home.vue';
+
 const history = createWebHistory();
 
 const router = createRouter({
@@ -9,8 +11,12 @@ const router = createRouter({
     routes : [
         {
             path: '/',
+            component: Home,
+        },
+        {
+            path: '/coming-soon',
             component: ComingSoon,
-        }
+        },
     ],
     scrollBehavior(to, from, savedPosition) {
         return { top: 0 } //always scroll to top
