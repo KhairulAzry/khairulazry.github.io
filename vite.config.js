e6aee36b856cbd0bfae3fd845319e63f15a9bafb
KhairulAzry <mkhairulazry@gmail.com>
2022-11-11 14:37:20 +0800

    Add comments for cname guide

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/'
+  // if got CNAME or deploying to https://<USERNAME>.github.io/
+  base: '/',
+  // if deploying to https://<USERNAME>.github.io/<REPO>/
+  // base: '/<REPO>/',
 })
